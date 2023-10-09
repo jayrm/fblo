3f0b34d0771c1bad5ceee613fa83ab7d8228fec0
coderJeff <coder@execulink.com>
2023-10-09 02:28:42 -0400

    build: allow mk-pkgs.bat to build one selected fbc version

--- mk-pkgs.bat
+++ mk-pkgs.bat
@@ -3,12 +3,13 @@ setlocal
 
 set DOCLEAN=
 set DOPACKAGE=
+set DOFBCVERSION=
+set DOTOOLCHAIN=
 
 @rem TODO:
-@rem   - maybe move this to a bash script
+@rem   - maybe move this to a bash script?
 @rem   - allow setting a package (to allow updating one package only)
 @rem   - configuration file?  paths are user system dependant
-@rem   - deduplicate commands? but maybe that makes it even more confusing...
 
 	set FBFROG=d:/fbfrog.git/fbfrog.exe
 
@@ -22,17 +23,23 @@ set DOPACKAGE=
 	if /I "%1" == "-h"      goto HELP
 	if /I "%1" == "--help"  goto HELP
 
-	if /i "%1" == "--clean"      call :SETCLEAN
-	if /i "%1" == "clean"        call :SETCLEAN
-	if /i "%1" == "--keep-build" call :SETKEEPBUILD 
-	if /i "%1" == "keep-build"   call :SETKEEPBUILD
+	if /i "%1" == "--clean"      goto SETCLEAN
+	if /i "%1" == "clean"        goto SETCLEAN
+	if /i "%1" == "--keep-build" goto SETKEEPBUILD
+	if /i "%1" == "keep-build"   goto SETKEEPBUILD
+
+	if /i "%1" == "fbc-1.10.0" goto SETFBCVERSION %1
+	if /i "%1" == "fbc-1.20.0" goto SETFBCVERSION %1
+
+	if /i "%1" == "mingw-w64-gcc-11.2.0"  goto SETTOOLCHAIN  %1
+	if /i "%1" == "winlbs-gcc-9.3.0"      goto SETTOOLCHAIN  %1
+	if /i "%1" == "mingw-w64-gcc-5.2.0"   goto SETTOOLCHAIN  %1
 
 	if NOT "%1" == "" set DOPACKAGE=%1
 
 :NEXTARG
 	shift
 	if "%1" == "" goto STARTBUILD
-
 	goto GETARGS
 
 :SETCLEAN
@@ -43,63 +50,91 @@ set DOPACKAGE=
 	set DOCLEAN=keep-build
 	goto NEXTARG
 
+:SETFBCVERSION
+	set DOFBCVERSION=%1
+	goto NEXTARG
+
+:SETTOOLCHAIN
+	set DOTOOLCHAIN=%1
+	goto NEXTARG
+
 @Rem ======================================================
 :STARTBUILD
 	@rem default to clean-build (./build.sh should choose it anyway)
 	if "%DOCLEAN%" == "" set DOCLEAN=clean-build
 
+	if "%DOFBCVERSION%" == "" goto BUILDALL
+	if "%DOFBCVERSION%" == "fbc-1.20.0" call :BUILD120
+	if "%DOFBCVERSION%" == "fbc-1.10.0" call :BUILD110
+	goto DONE
+
+:BUILDALL
 	call :BUILD120
 	call :BUILD110
-
 	goto DONE
 
 @Rem ======================================================
-:BUILD120
+:BUILD110
+	set FBC32=d:/fb.1.10/fbc-win32.exe
+	set FBC64=d:/fb.1.10/fbc-win64.exe
 
+	call :BUILD32 fbc-1.10.0
+	call :BUILD64 fbc-1.10.0
+	exit /b
+
+@Rem ======================================================
+:BUILD120
 	set FBC32=d:/fb.git/fbc-win32.exe
 	set FBC64=d:/fb.git/fbc-win64.exe
 
-	set PATH=%MSYS32PATHS%
-	set ARGS=win32 --fbfrog %FBFROG% --fbc %FBC32% fbc-1.20.0 %DOCLEAN% %DOPACKAGE%
+	call :BUILD32 fbc-1.20.0
+	call :BUILD64 fbc-1.20.0
+	exit /b
 
-	sh -c "./mk-pkg.sh %ARGS% winlibs-gcc-9.3.0 --prefix c:/winlibs-9.3"
-	sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-5.2.0 --prefix c:/mingw-w64-5.2"
-	sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-11.2.0 --prefix c:/mingw-w64-11.2"
+@Rem ======================================================
+:SETTOOLCHAINS
+	set dowinlibs930=N
+	set domingw520=N
+	set domingw1120=N
 
-	set PATH=%MSYS64PATHS%
-	set ARGS=win64 --fbfrog %FBFROG% --fbc %FBC64% fbc-1.20.0 %DOCLEAN% %DOPACKAGE%
+	if /i "%DOTOOLCHAIN%" == "" set domingw1120=Y
+	if /i "%DOTOOLCHAIN%" == "" set dowinlibs930=Y
+	if /i "%DOTOOLCHAIN%" == "" set domingw520=Y
 
-	sh -c "./mk-pkg.sh %ARGS% winlibs-gcc-9.3.0 --prefix c:/winlibs-9.3"
-	sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-5.2.0 --prefix c:/mingw-w64-5.2"
-	sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-11.2.0 --prefix c:/mingw-w64-11.2"
+	if /i "%DOTOOLCHAIN%" == "mingw-w64-gcc-11.2.0" set domingw1120=Y
+	if /i "%DOTOOLCHAIN%" == "winlibs-gcc-9.3.0" set dowinlibs930=Y
+	if /i "%DOTOOLCHAIN%" == "mingw-w64-gcc-5.2.0" set domingw520=Y
 
 	exit /b
 
 @Rem ======================================================
-:BUILD110
-
-	set FBC32=d:/fb.1.10/fbc-win32.exe
-	set FBC64=d:/fb.1.10/fbc-win64.exe
-
+:BUILD32
 	set PATH=%MSYS32PATHS%
-	set ARGS=win32 --fbfrog %FBFROG% --fbc %FBC32% fbc-1.10.0 %DOCLEAN% %DOPACKAGE%
+	set ARGS=win32 --fbfrog %FBFROG% --fbc %FBC32% %1 %DOCLEAN% %DOPACKAGE%
 
-	sh -c "./mk-pkg.sh %ARGS% winlibs-gcc-9.3.0 --prefix c:/winlibs-9.3"
-	sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-5.2.0 --prefix c:/mingw-w64-5.2"
-	sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-11.2.0 --prefix c:/mingw-w64-11.2"
+	call :SETTOOLCHAINS
 
+	if /i "%domingw1120%"  == "Y" sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-11.2.0 --prefix c:/mingw-w64-11.2"
+	if /i "%dowinlibs930%" == "Y" sh -c "./mk-pkg.sh %ARGS% winlibs-gcc-9.3.0 --prefix c:/winlibs-9.3"
+	if /i "%domingw520%"   == "Y" sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-5.2.0 --prefix c:/mingw-w64-5.2"
+
+	exit /b
+
+:BUILD64
 	set PATH=%MSYS64PATHS%
-	set ARGS=win64 --fbfrog %FBFROG% --fbc %FBC64% fbc-1.10.0 %DOCLEAN% %DOPACKAGE%
+	set ARGS=win64 --fbfrog %FBFROG% --fbc %FBC64% %1 %DOCLEAN% %DOPACKAGE%
 
-	sh -c "./mk-pkg.sh %ARGS% winlibs-gcc-9.3.0 --prefix c:/winlibs-9.3"
-	sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-5.2.0 --prefix c:/mingw-w64-5.2"
-	sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-11.2.0 --prefix c:/mingw-w64-11.2"
+	call :SETTOOLCHAINS
+
+	if /i "%domingw1120%"  == "Y" sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-11.2.0 --prefix c:/mingw-w64-11.2"
+	if /i "%dowinlibs930%" == "Y" sh -c "./mk-pkg.sh %ARGS% winlibs-gcc-9.3.0 --prefix c:/winlibs-9.3"
+	if /i "%domingw520%"   == "Y" sh -c "./mk-pkg.sh %ARGS% mingw-w64-gcc-5.2.0 --prefix c:/mingw-w64-5.2"
 
 	exit /b
 
 @Rem ======================================================
 :HELP
-	echo mk-pkgs.bat [options] [packageid]
+	echo mk-pkgs.bat [options] [packageid] [fbcversion]
 	echo --clean
 	echo --keep-build
 	echo see ./build.sh for package ids
